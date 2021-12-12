286c439358f2c6e56dfdd106a39892c784442572
Vitória Toebe <vitoriatoebe@gmail.com>
2021-12-12 19:53:51 -0300

    Create GuestList.java

---
+++ src/com/vtoebe/arrays_loops/guest_list/GuestList.java
@@ -0,0 +1,10 @@
+package com.vtoebe.arrays_loops.guest_list;
+
+import com.vtoebe.arrays_loops.guest_list.service.LongestNameFinder;
+
+public class GuestList {
+    public static void main(String[] args) {
+        LongestNameFinder find = new LongestNameFinder();
+
+    }
+}
