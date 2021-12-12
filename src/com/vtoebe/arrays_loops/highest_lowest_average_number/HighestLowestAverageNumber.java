8613fefbc4a20d1dbf85238e50f5500f97bf97b1
Vitória Toebe <vitoriatoebe@gmail.com>
2021-12-12 19:53:00 -0300

    add: continuity checker

--- src/com/vtoebe/arrays_loops/highest_lowest_average_number/HighestLowestAverageNumber.java
+++ src/com/vtoebe/arrays_loops/highest_lowest_average_number/HighestLowestAverageNumber.java
@@ -1,15 +1,18 @@
 package com.vtoebe.arrays_loops.highest_lowest_average_number;
 
-
 import com.vtoebe.arrays_loops.highest_lowest_average_number.utils.HighLowAvgPrinter;
 import com.vtoebe.arrays_loops.highest_lowest_average_number.utils.HighLowAvgWriter;
+import com.vtoebe.arrays_loops.service.ContinuityChecker;
 
 public class HighestLowestAverageNumber {
     public static void main(String[] args) {
+        ContinuityChecker continuity = new ContinuityChecker();
         HighLowAvgPrinter print = new HighLowAvgPrinter();
         HighLowAvgWriter write = new HighLowAvgWriter();
 
-        write.inputNumbers(print.numberRequest());
-        print.HighLowAvgResult(write.HighLowAvgFound());
+        do{
+            write.inputNumbers(print.numberRequest());
+            print.HighLowAvgResult(write.HighLowAvgFound());
+        } while (continuity.checkHighLowAvg());
     }
 }
