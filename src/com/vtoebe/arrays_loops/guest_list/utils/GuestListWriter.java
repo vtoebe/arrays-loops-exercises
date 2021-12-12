8338535f55b70244e2ec5f82e6dfbb98437380d6
Vitória Toebe <vitoriatoebe@gmail.com>
2021-12-12 20:59:35 -0300

    Update GuestListWriter.java

--- src/com/vtoebe/arrays_loops/guest_list/utils/GuestListWriter.java
+++ src/com/vtoebe/arrays_loops/guest_list/utils/GuestListWriter.java
@@ -1,8 +1,25 @@
 package com.vtoebe.arrays_loops.guest_list.utils;
 
+import com.vtoebe.arrays_loops.guest_list.service.ListHandler;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class GuestListWriter {
     Scanner scan = new Scanner(System.in);
+    ListHandler list = new ListHandler();
+
+    private ArrayList<String> guestList = new ArrayList<String>();
+
+    public ArrayList<String> getGuestList() { return guestList; }
+    public int getListSize(){ return guestList.size(); }
+
+    public void addGuestToList(){
+        String name = scan.nextLine();
+        if (name.toUpperCase().equals("STOP")){
+            list.stopAddingNames();
+        } else { guestList.add(name); }
+
+    }
 
 }
