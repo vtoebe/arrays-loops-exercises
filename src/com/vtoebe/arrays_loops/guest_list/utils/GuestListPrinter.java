c4a47379ebc92358c92eccc2f9ff838501bb74db
Vitória Toebe <vitoriatoebe@gmail.com>
2021-12-12 21:00:56 -0300

    add: printer functions

--- src/com/vtoebe/arrays_loops/guest_list/utils/GuestListPrinter.java
+++ src/com/vtoebe/arrays_loops/guest_list/utils/GuestListPrinter.java
@@ -1,4 +1,13 @@
 package com.vtoebe.arrays_loops.guest_list.utils;
 
 public class GuestListPrinter {
+    final String NAME_REQUEST = "Add a name to the guest list: ";
+    final String SIZE_CHECK = "Your list has to have at least five names: ";
+    final String KEEP_ADDING_NAMES = "If you want to stop adding names, type STOP: ";
+    final String LONGEST_NAME = "The longest name in the list is: ";
+
+    public void nameRequest(){ System.out.print(NAME_REQUEST); }
+    public void wrongSize(){ System.out.println(SIZE_CHECK); }
+    public void keepAddingNames(){ System.out.println(KEEP_ADDING_NAMES); }
+    public void longestName(String name){ System.out.println(LONGEST_NAME + name); }
 }
