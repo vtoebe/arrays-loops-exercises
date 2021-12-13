ad1a1b0d89997058b6aeabb48247b80cae57889e
Vitória Toebe <vitoriatoebe@gmail.com>
2021-12-12 23:49:39 -0300

    add: imc printer

---
+++ src/com/vtoebe/arrays_loops/check_imc/utils/IMCPrinter.java
@@ -0,0 +1,13 @@
+package com.vtoebe.arrays_loops.check_imc.utils;
+
+public class IMCPrinter {
+
+    final String REQUEST_NAME = "Please, type the person's name: ";
+    final String REQUEST_HEIGHT = "Please, type the person's height: ";
+    final String REQUEST_WEIGHT = "Please, type the person's weight: ";
+
+    public void requestName(){ System.out.print(REQUEST_NAME); }
+    public void requestHeight(){ System.out.print(REQUEST_HEIGHT); }
+    public void requestWeight(){ System.out.print(REQUEST_WEIGHT); }
+
+}
