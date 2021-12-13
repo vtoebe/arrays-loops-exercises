bdf52f6a9e6ac6a3e8ca88d39041be159069bd67
Vitória Toebe <vitoriatoebe@gmail.com>
2021-12-13 02:31:35 -0300

    Refactor: remove continuity checker | change class calls

--- src/com/vtoebe/arrays_loops/number_separator/NumberSeparator.java
+++ src/com/vtoebe/arrays_loops/number_separator/NumberSeparator.java
@@ -3,19 +3,20 @@ package com.vtoebe.arrays_loops.number_separator;
 import com.vtoebe.arrays_loops.number_separator.service.Separator;
 import com.vtoebe.arrays_loops.number_separator.utils.SeparateNumbersPrinter;
 import com.vtoebe.arrays_loops.number_separator.utils.SeparateNumbersWriter;
-import com.vtoebe.arrays_loops.service.ContinuityChecker;
 
 public class NumberSeparator {
     public static void main(String[] args){
-        ContinuityChecker continuity = new ContinuityChecker();
 
         SeparateNumbersPrinter print = new SeparateNumbersPrinter();
         SeparateNumbersWriter write = new SeparateNumbersWriter();
         Separator separate = new Separator();
 
+            print.requestNumbers();
+            write.setInputNumbers();
 
-            write.setInputNumbers(print.requestNumbers());
-            separate.separateAllNumbers(write.getInputNumbers());
+        separate.separateAllNumbers(write.getInputNumbers());
+
+
 
     }
 }
