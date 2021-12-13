b5ef6ab70b3db0ff1acf252f12934e40824a3319
Vitória Toebe <vitoriatoebe@gmail.com>
2021-12-13 02:35:32 -0300

    Refactor: simplify and cleanup
    removed classes with repeated code and simplified for loop code

--- src/com/vtoebe/arrays_loops/number_separator/service/Separator.java
+++ src/com/vtoebe/arrays_loops/number_separator/service/Separator.java
@@ -1,41 +1,20 @@
 package com.vtoebe.arrays_loops.number_separator.service;
 
-import com.vtoebe.arrays_loops.number_separator.utils.SeparateNumbersWriter;
+import com.vtoebe.arrays_loops.number_separator.utils.SeparateNumbersPrinter;
 
 import java.util.ArrayList;
 
 public class Separator {
-    SeparateNumbersWriter w = new SeparateNumbersWriter();
-    private final ArrayList<Integer> oddNumbers = new ArrayList<Integer>(0);
-    private final ArrayList<Integer> evenNumbers = new ArrayList<Integer>(0);
+    SeparateNumbersPrinter print = new SeparateNumbersPrinter();
+    private ArrayList<Integer> oddNumbers = new ArrayList<>();
+    private ArrayList<Integer> evenNumbers = new ArrayList<>();
 
-    public void separateOdd(ArrayList<Integer> inputNumbers){
-        for (int number:inputNumbers) {
-            if (number%2!=0){
-                oddNumbers.add(number);
-            }
+    public void separateAllNumbers(int[] inputNumbers){
+        for(int number : inputNumbers){
+            if (number % 2 != 0){ oddNumbers.add(number); }
+
+            if (number % 2 == 0){ evenNumbers.add(number); }
         }
-        System.out.println(oddNumbers);
-        w.setOddNumbers(oddNumbers);
-    }
-
-    public void separateEven(ArrayList<Integer> inputNumbers){
-        for (int number:inputNumbers) {
-            if (number%2==0){
-                evenNumbers.add(number);
-            }
-        }
-        w.setEvenNumbers(evenNumbers);
-
-        for (int number:evenNumbers
-             ) {
-            System.out.println(number);
-        }
-        System.out.println(evenNumbers);
-    }
-
-    public void separateAllNumbers(ArrayList<Integer> numbers){
-            separateOdd(numbers);
-            separateEven(numbers);
+        print.separationResult(oddNumbers, evenNumbers);
     }
 }
